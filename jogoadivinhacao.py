d9e463276bd62df05552a8ed0704915a540fb63a
rafah <136349907+rafah051@users.noreply.github.com>
2024-03-13 09:42:56 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -7,21 +7,25 @@ numeroSecreto = 38
 
 #Definindo o número de tentativas
 numeroTentativas = 3
+rodada = 1
 
-while(numeroTentativas > 0):
-    print('Ok')
+while(rodada <= numeroTentativas):
+    print('tentativa', rodada,'de'  , numeroTentativas)
 
 #Recebendo o chute do jogador
     chuteString = input('Digite um número: ')
-    print('Você digitou o número', chuteString)
     chute = int(chuteString)
 
 #Declarando as condições
     if numeroSecreto == chute:
         print('Você acertou!')
+        break
     elif(chute>numeroSecreto):
         print('Você errou!! O número secreto é um número menor')
     else:
         print('Você errou!! O número secreto é um número maior')
+
+    #numeroTentativas = numeroTentativas -1
+    rodada = rodada +1
 
 #Aula Elif 26.02.24
