6d8ae2818494652e3fe55b2692f41e3f941f1165
rafah <136349907+rafah051@users.noreply.github.com>
2024-03-27 08:56:07 -0300

    Add files via upload

--- jogoadivinhacao.py
+++ jogoadivinhacao.py
@@ -1,19 +1,20 @@
+import random;
+
 print('*********************************')
 print('Bem vindo, ao JOGO DE ADIVINHAÇÃO')
 print('*********************************')
 
 #Definindo o número secreto
-numeroSecreto = 38
+numeroSecreto = round(random.random()*100)
 
 #Definindo o número de tentativas
 numeroTentativas = 3
 rodada = 1
-
 while(rodada <= numeroTentativas):
     print('tentativa', rodada,'de'  , numeroTentativas)
 
 #Recebendo o chute do jogador
-    chuteString = input('Digite um número: ')
+    chuteString = input('Digite um número entre 1 e 100: ')
     chute = int(chuteString)
 
 #Declarando as condições
