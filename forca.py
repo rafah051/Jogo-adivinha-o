4db447b0b6c8a62e88e6344aca74b58df6750c38
rafah <136349907+rafah051@users.noreply.github.com>
2024-06-05 09:01:34 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -1,7 +1,25 @@
 def jogar_forca():
 
-    print('*************')
+    print('*********************************')
     print('Bem vindo, ao JOGO FORCA')
-    print("***************")
-   
-    print("fim de jogo")
+    print('*********************************')
+    #Definir qual a palavra secreta
+    palavraSecreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
+    while(not enforcou and not acertou):
+        
+        chute = input ('Qual a letra?')
+
+        for letra in palavraSecreta:
+            if (chute == letra):
+                print(letra)
+        print("Jogando....")
+
+
+    print("Fim de jogo!")
+if(__name__ == "__main__"):
+    jogar_forca()
--- jogos.py
+++ jogos.py
@@ -1,19 +1,22 @@
 import forca
 import jogoadivinhacao
 
-print("************************************")
-print('********Escolha o seu jogo!*********')
-print("************************************")
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
 
-print("(1) Forca (2) Adivinhação")
+    print("(1) Forca (2) Adivinhação")
 
-jogo = int(input("Qual jogo?"))
+    jogo = int(input("Qual jogo?"))
 
-#CRIANDO A CONDIÇÃO
+    #criando a condição
+    if(jogo == 1):
+        print("Jogando jogo Forca")
+        forca.jogar_forca()
+    else:
+        print("Jogando jogo da Adivinhação")
+        jogoadivinhacao.jogar_adivinhacao()
 
-if(jogo == 1):
-    print('Jogando jogo Forca')
-    forca.jogar_forca()
-else:
-    print('Jogando jogo da Adivinhação')
-    jogoadivinhacao.jogar_adivinhação()
+if(__name__ == "__main__"):
+    escolhe_jogo()
